f67013dd752dd88b9750a1b25e5b1cab7a13e014
bhanurz <aswanggab@gmail.com>
2024-07-24 13:10:35 +0700

    feat: add static data for radio button company setting

---
+++ src/data/company-setting.tsx
@@ -0,0 +1,19 @@
+export const paymentStatus = [
+  { label: "Active", value: true },
+  { label: "Inactive", value: false },
+];
+
+export const marginShare = [
+  { label: "Nominal", value: "share" },
+  { label: "Percentage", value: "share_percentage" },
+];
+
+export const statusCompany = [
+  { label: "Active", value: true },
+  { label: "Inactive", value: false },
+];
+
+export const withdrawalStatus = [
+  { label: "Active", value: true },
+  { label: "Inactive", value: false },
+];
