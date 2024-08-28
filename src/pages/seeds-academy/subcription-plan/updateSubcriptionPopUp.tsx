2ad2fc71e20328ace5e4f395e4ea3e0d260f17d5
HusinHanbasy <husinhanbasy88@gmail.com>
2024-08-28 14:32:18 +0700

    fix code review

--- src/pages/seeds-academy/subcription-plan/updateSubcriptionPopUp.tsx
+++ src/pages/seeds-academy/subcription-plan/updateSubcriptionPopUp.tsx
@@ -11,59 +11,7 @@ import ReactSelect, { GroupBase } from "react-select";
 import { statusSubcription } from "data/seeds-academy";
 import useRNCHelper from "hooks/shared/useRNCHelper";
 import { useGetSubscriptionByIdQuery } from "services/modules/seeds-academy";
-
-const optionQuestion = [
-  {
-    key: 1,
-    label: "1 bulan",
-    value: "1",
-  },
-  {
-    key: 1,
-    label: "2 bulan",
-    value: "2",
-  },
-  {
-    key: 1,
-    label: "3 bulan",
-    value: "3",
-  },
-  {
-    key: 1,
-    label: "4 bulan",
-    value: "4",
-  },
-  {
-    key: 1,
-    label: "5 bulan",
-    value: "5",
-  },
-  {
-    key: 1,
-    label: "6 bulan",
-    value: "6",
-  },
-  {
-    key: 1,
-    label: "9 bulan",
-    value: "9",
-  },
-  {
-    key: 1,
-    label: "12 bulan",
-    value: "12",
-  },
-  {
-    key: 1,
-    label: "18 bulan",
-    value: "18",
-  },
-  {
-    key: 1,
-    label: "24 bulan",
-    value: "24",
-  },
-];
+import { optionQuestion } from "../../../data/seeds-academy";
 
 const UpdateSubcriptionPopUp: React.FC<{
   isOpen: boolean;
