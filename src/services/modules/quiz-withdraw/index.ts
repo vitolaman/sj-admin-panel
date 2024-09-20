faf0ce2462ef47e6517c0861fc7b5c90c60fa935
bhanurz <aswanggab@gmail.com>
2024-09-20 15:11:22 +0700

    refactor: api call for get quiz withdraw list

--- src/services/modules/quiz-withdraw/index.ts
+++ src/services/modules/quiz-withdraw/index.ts
@@ -8,8 +8,10 @@ import { Api } from "services/api";
 export const quizWithdrawApi = Api.injectEndpoints({
   endpoints: (build) => ({
     quizWithdrawList: build.query<QuizWithdrawResI, QuizWithdrawReqI>({
-      query: (params) =>
-        `quiz/v1/withdraw/list?search=${params.search}&status=${params.status}&page=${params.page}&limit=${params.limit}`,
+      query: (params) => ({
+        url: `quiz/v1/withdraw/list`,
+        params,
+      }),
       keepUnusedDataFor: 0,
     }),
     quizUpdateWithdrawStatus: build.mutation<void, QuizWithdrawUpdateI>({
